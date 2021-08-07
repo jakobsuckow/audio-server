b8032895d588935629deca1de70370b17f56dbb0
Jakob Suckow <jakob@martian.agency>
2021-08-07 17:48:15 +0200

    added typeorm

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,31 @@ import { ConfigModule } from "@nestjs/config";
 import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
 import { BlobModule } from "./components/blob/blob.module";
+import { TypeOrmModule } from "@nestjs/typeorm";
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true }), BlobModule],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRoot({
+      type: "postgres",
+      host: process.env.DATABASE_HOST,
+      port: 5432,
+      username: process.env.DATABASE_USER,
+      password: process.env.DATABASE_PASSWORD,
+      database: process.env.DATABASE_NAME,
+      entities: [
+        "../dist/components/**/**.entity{.ts,.js}",
+        "../node_modules/nestjs-admin/**/*.entity.js"
+      ],
+      migrations: ["dist/migrations/**.js"],
+      synchronize: process.env.NODE_ENV === "production" ? false : true,
+      autoLoadEntities: true,
+      ssl: {
+        rejectUnauthorized: false
+      }
+    }),
+    BlobModule
+  ],
   controllers: [AppController],
   providers: [AppService]
 })

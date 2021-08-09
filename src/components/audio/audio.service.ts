848b1a9e63e8aa697627d954f2022c0274c229e0
Jakob Suckow <jakob@martian.agency>
2021-08-09 13:02:57 +0200

    added ffmpeg

--- src/components/audio/audio.service.ts
+++ src/components/audio/audio.service.ts
@@ -4,14 +4,20 @@ import { Repository } from "typeorm";
 import { Blob, CreateBlobDto } from "../blob/blob.dto";
 import { AzureBlobService } from "../blob/blob.service";
 import { AudioEntity } from "./audio.entity";
+import { FfmpegCommand } from "fluent-ffmpeg";
+import Ffmpeg = require("fluent-ffmpeg");
+import { Readable } from "stream";
 
 @Injectable()
 export class AudioService {
+  ffmpeg: FfmpegCommand;
   constructor(
     @InjectRepository(AudioEntity)
     private audioRepository: Repository<AudioEntity>,
     readonly azureBlobService: AzureBlobService
-  ) {}
+  ) {
+    this.ffmpeg = new FfmpegCommand();
+  }
 
   async create(blob: CreateBlobDto) {
     const res = await this.azureBlobService.createFromStream(blob);
@@ -26,6 +32,22 @@ export class AudioService {
     return this.azureBlobService.downloadFile(
       `${String(entity.fieldname)}.${entity.mimetype.split("/")[1]}`
     );
+  }
+
+  async analyze(id: string): Promise<Ffmpeg.FfprobeData> {
+    const entity = await this.audioRepository.findOne(id);
+    const blob = await this.azureBlobService.downloadFile(
+      `${String(entity.fieldname)}.${entity.mimetype.split("/")[1]}`
+    );
+
+    return new Promise((resolve, reject) => {
+      this.ffmpeg.input(Readable.from(blob)).ffprobe((err, data) => {
+        if (err) {
+          reject(err);
+        }
+        resolve(data);
+      });
+    });
   }
 
   async createURL(entity: AudioEntity) {

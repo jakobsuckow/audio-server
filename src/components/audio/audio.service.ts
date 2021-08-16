276c74dddd4e9292303148f906355787ecafcba1
Jakob Suckow <jakob@martian.agency>
2021-08-16 16:56:29 +0200

    added ffmpeg

--- src/components/audio/audio.service.ts
+++ src/components/audio/audio.service.ts
@@ -5,22 +5,34 @@ import { CreateBlobDto } from "../blob/blob.dto";
 import { AzureBlobService } from "../blob/blob.service";
 import { AudioEntity } from "./audio.entity";
 import { getAudioDurationInSeconds } from "get-audio-duration";
-import { Readable } from "stream";
+import { Readable, Stream } from "stream";
 import { WatsonService } from "../watson/watson.service";
+import { Lame } from "node-lame";
+import * as Ffmpeg from "fluent-ffmpeg";
+import { WinstonService } from "../winston/winston.service";
 
 @Injectable()
 export class AudioService {
+  lame: Lame;
   constructor(
     @InjectRepository(AudioEntity)
     private audioRepository: Repository<AudioEntity>,
     readonly azureBlobService: AzureBlobService,
-    private watsonService: WatsonService
-  ) {}
+    private watsonService: WatsonService,
+    private logger: WinstonService
+  ) {
+    this.lame = new Lame({ output: "buffer", bitrate: 320 });
+  }
 
   // TODO: Should be createAudioDTO
 
   async create(blob: CreateBlobDto) {
-    const res = await this.azureBlobService.upload(blob);
+    const mp3 = await this.toMp3(blob.buffer);
+    const newFile = {
+      ...blob,
+      buffer: mp3
+    };
+    const res = await this.azureBlobService.upload(newFile);
     if (res) {
       const savedEntity = await this.audioRepository.save(blob);
       return savedEntity;
@@ -45,5 +57,19 @@ export class AudioService {
 
   async getDuration(stream: Readable): Promise<number> {
     return await getAudioDurationInSeconds(stream);
+  }
+
+  async toMp3(buffer: Buffer): Promise<Buffer> {
+    const encoder = this.lame.setBuffer(buffer);
+    const res = await encoder.encode();
+    if (res) {
+      return encoder.getBuffer();
+    }
+  }
+
+  async ffmpegToMp3(stream: Readable) {
+    return Ffmpeg(stream)
+      .format("mp3")
+      .save("./test.mp3");
   }
 }
